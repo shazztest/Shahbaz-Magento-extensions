3b6429689481416a6c9a03c8c0e35701d5fe7ddc
ubuntu <ubuntu@mysys.myguest.virtualbox.org>
2024-07-13 22:21:13 +0530

    Custom Theme

---
+++ Custom_Theme/PacifiersRus/default/Magento_Review/web/js/process-reviews.js
@@ -0,0 +1,64 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'jquery'
+], function ($) {
+    
+
+    /**
+     * @param {String} url
+     * @param {*} fromPages
+     */
+    function processReviews(url, fromPages) {
+        $.ajax({
+            url: url,
+            cache: true,
+            dataType: 'html',
+            showLoader: false,
+            loaderContext: $('.product.data.items')
+        }).done(function (data) {
+            $('#product-review-container').html(data).trigger('contentUpdated');
+            $('[data-role="product-review"] .pages a').each(function (index, element) {
+                $(element).on('click', function (event) { //eslint-disable-line max-nested-callbacks
+                    processReviews($(element).attr('href'), true);
+                    event.preventDefault();
+                });
+            });
+        }).always(function () {
+            if (fromPages == true) { //eslint-disable-line eqeqeq
+                $('html, body').animate({
+                    scrollTop: $('#reviews').offset().top - 50
+                }, 300);
+            }
+        });
+    }
+
+    return function (config) {
+        processReviews(config.productReviewUrl);
+
+        $(function () {
+            $('.product-info-main .reviews-actions a').on('click', function (event) {
+                var anchor, addReviewBlock;
+
+                event.preventDefault();
+                anchor = $(this).attr('href').replace(/^.*?(#|$)/, '');
+                addReviewBlock = $('#' + anchor);
+
+                if (addReviewBlock.length) {
+                    $('.product.data.items [data-role="content"]').each(function (index) { //eslint-disable-line
+                        if (this.id == 'reviews') { //eslint-disable-line eqeqeq
+                            $('.product.data.items').tabs('activate', index);
+                        }
+                    });
+                    $('html, body').animate({
+                        scrollTop: addReviewBlock.offset().top - 50
+                    }, 300);
+                }
+
+            });
+        });
+    };
+});
